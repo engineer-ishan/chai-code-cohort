b8c6e2b1d00fc6e041c177a43b33b4d922e68d21
Ishan Chandrakar <ishanchandrakar228@gmail.com>
2025-12-17 10:21:13 +0530

    learned class

---
+++ Class_Code/js_codes/Objects_Prototypes/class.js
@@ -0,0 +1,13 @@
+class Person{
+    constructor(fname, lname){
+        this.fname = fname
+        this.lname = lname
+    }
+    getFullName(){
+        return `${this.fname} ${this.lname}`
+    }
+}
+const p1 = new Person('piyus','garg')
+const p2 = new Person('hitesh','choudhary')
+
+console.log(p2.getFullName());
--- Class_Code/js_codes/Objects_Prototypes/polyfill.js
+++ Class_Code/js_codes/Objects_Prototypes/polyfill.js
@@ -34,17 +34,51 @@ const arr = [1,2,3,4,5,6,7,8,9,10]
 // filter functionn
 // signature - return new array, input - userFn,
 
-if(!Array.prototype.myFilter){
-    Array.prototype.myFilter = function(userFn){
-        const filterArray = []
-        for(let i=0; i<this.length; i++){
+// if(!Array.prototype.myFilter){
+//     Array.prototype.myFilter = function(userFn){
+//         const filterArray = []
+//         for(let i=0; i<this.length; i++){
 
-            if(userFn(this[i]))
-                filterArray.push(this[i])
-        }
-        return filterArray
+//             if(userFn(this[i]))
+//                 filterArray.push(this[i])
+//         }
+//         return filterArray
+//     }
+// }
+
+// const myFilterArr = arr.myFilter((e)=> e%2==1 )
+// console.log(myFilterArr);
+
+// reduce function
+// signature - return a single value, accumulator, currentValue and initial value are passed
+
+// if(!Array.prototype.myReduce){
+//     Array.prototype.myReduce = function(userFn, initials){
+//         let acc = initials
+//         for(let i=0; i<this.length; i++){
+//             acc = userFn(acc, this[i])
+//         }
+//         console.log(acc);
+        
+//         return acc
+//     }
+// }
+
+// const initialValue = 0
+// const sum = arr.myReduce((acc, curr) => acc + curr, 0);
+// console.log(sum); 
+
+// max function
+// signature - return max number in the array
+
+if(!Array.prototype.max){
+    Array.prototype.max = function(){
+        let maximum = -Infinity
+        for(let num of this)
+            if(num>maximum)
+                maximum = num
+        return maximum
     }
 }
-
-const myFilterArr = arr.myFilter((e)=> e%2==1 )
-console.log(myFilterArr);
+const maxOfArray = arr.max()
+console.log(maxOfArray);
